d8bed80d4835ceb633c4ff1578b52a03dcd70337
Adam Jankanič <xjankanic@stuba.sk>
2023-05-10 09:40:56 +0200

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-const https = require("https");
+app.use(cors());
+
+// Rest of your code...
 const server = https.createServer(app);
 
 app.get("/", (req, res) => {
