049e8c4ceb72c4f3b9e87d084d20b8dda959c2ea
Adam Jankanič <xjankanic@stuba.sk>
2023-05-10 10:05:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get("/", (req, res) => {
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log("listening on *:" + port);
 });
